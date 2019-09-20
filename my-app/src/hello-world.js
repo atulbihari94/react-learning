6ac70557a9135c258c462dcfbb1e812c9e08bfdd
sudhir <sudhir@workplacefabric.com>
2019-09-20 11:11:09 +0530

    package lock in ignored

--- my-app/src/hello-world.js
+++ my-app/src/hello-world.js
@@ -1,11 +1,13 @@
 import React from 'react';
 const helloWorld = () => {
+    const pStyle = {};
+    pStyle.color = "red";
     return (
-    <div>
-        <p style={{"color":"green"}}>Hi, I am Atul Bihari</p>
-        <p style={{"color":"blue"}}>Start Learning React JS from Scratch</p>
-        <h3 style={{"color":"pink"}}>Hello World</h3>
-    </div> );
+        <div>
+            <p style={{ "color": "green" }}>Hi, I am Atul Bihari</p>
+            <p style={{ "color": "blue" }}>Start Learning React JS from Scratch</p>
+            <h3 style={pStyle}>Hello World</h3>
+        </div>);
 }
 
 export default helloWorld;
