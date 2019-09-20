792b81072df90cc9229690dfd4ed9bcb1128c7ba
sudhir <sudhir@workplacefabric.com>
2019-09-20 18:33:08 +0530

    calender component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <div>
+          <Calendar
+            // onChange={this.onChange}
+            value={new Date()}
+          />
+        </div>
       </header>
     </div>
   );
