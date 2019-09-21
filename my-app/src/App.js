feccbb9747c02c39158a8bb2e44f3f940ca182be
sudhir <sudhir@workplacefabric.com>
2019-09-21 10:37:39 +0530

    calendar wiht button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
 import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+  const [buttonToggle, setButtonToggle] = useState(false);
+
+  const onChange = (date) => {
+    setDate(date);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <Calendar
-            // onChange={this.onChange}
-            value={new Date()}
-          />
-        </div>
+        <button onClick={() => setButtonToggle(!buttonToggle)}>
+          choose your date (this is toggle button)
+        </button>
+        {
+          buttonToggle ?
+            <div>
+              <Calendar
+                onChange={onChange}
+                value={date}
+              />
+              <h2>you are select the {'' + date.toDateString()}</h2>
+            </div>
+            : 'click on the button to show the calendar'}
+
       </header>
     </div>
   );
